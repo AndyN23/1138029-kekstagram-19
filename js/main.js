255ccff18a449d5c482b300cc0b1422d059ec404
AndyN23 <a.y.nesterov@yandex.ru>
2020-02-08 00:49:00 +0300

    На проверку2

--- js/main.js
+++ js/main.js
@@ -9,73 +9,78 @@ var MESSAGE = [
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 
 var USER_NAME = ['Дженифер Лорес', 'Юрий Бабарин', 'Брюс Чхи', 'Гай Мичи', 'Леонель Несси'];
-
 var PICTURE_COUNT = 25;
+var MIN_LIKES = 15;
+var MAX_LIKES = 200;
 
 // функции для получения случайного числа
-
 var getRandomValue = function (arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 };
 
+// Функция случайного числа в промежутке между мин и макс числом
 var getRandomInteger = function (min, max) {
   return Math.floor(min + Math.random() * (max + 1 - min));
 };
 
 var getRandomNotRepeat = function (min, max, num) {
-  var i, arr = [], res = [];
-  for (i = min; i <= max; i++) arr.push(i);
-  for (i = 0; i < num; i++) res.push(arr.splice(Math.floor(Math.random() * (arr.length)), 1)[0]);
+  var arr = [];
+  var res = [];
+  for (var i = min; i <= max; i++) {
+    arr.push(i);
+  }
+  for (i = 0; i < num; i++) {
+    res.push(arr.splice(Math.floor(Math.random() * (arr.length)), 1)[0]);
+  }
   return res;
 };
 
-// Генерируем массив из комментариев
-var userComments = [];
-for (var i = 1; i <= 8; i++) {
-  userComments.push(
-      {avatar: 'img/avatar-' + getRandomInteger(1, 6) + '.svg',
-        message: getRandomValue(MESSAGE),
-        name: getRandomValue(USER_NAME)
-      });
-}
+// Функция для создания массива комментариев
+var getRandomComments = function (commentsArray) {
+  var res = [];
+  var randomCount = getRandomInteger(1, 5);
+  for (i = 0; i < randomCount; i++) {
+    res.push(
+        {
+          avatar: 'img/avatar-' + getRandomInteger(1, 6) + '.svg',
+          message: getRandomValue(MESSAGE),
+          name: getRandomValue(USER_NAME)
+        });
+  }
+  return res;
+};
 
-var picturesElement = document.querySelector('.pictures'); // Вставляем фото в блок c этим классом
+// Создаем и заполняем массив из фото - переделать в функцию
+var createUsersPhoto = function (userPhotos) {
+  for (var i = 1; i <= PICTURE_COUNT; i++) {
+    userPhotos.push(
+        {url: 'photos/' + getRandomNotRepeat(1, PICTURE_COUNT, 1) + '.jpg',
+          description: 'Описание фотографии',
+          likes: getRandomInteger(MIN_LIKES, MAX_LIKES),
+          comments: getRandomComments(commentsArray)
+        });
+  }
+  return userPhotos;
+};
+
+
+
+
+var picturesListElement = document.querySelector('.pictures'); // Вставляем фото в блок c этим классом
 var pictureTemplate = document.querySelector('#picture')
     .content
     .querySelector('.picture'); // Копируемый темплейт с данным классом
 
-// Генерируем массив из фото
-var createUsersPhotos = function (arrPhotos) {
-  for (i = 1; i <= PICTURE_COUNT; i++) {
-    arrPhotos.push(
-        {url: 'photos/' + getRandomNotRepeat(1, PICTURE_COUNT, 1) + '.jpg',
-          description: 'Описание фотографии',
-          likes: getRandomInteger(15, 200),
-          comments: getRandomValue(userComments)
-        });
-  }
-  return arrPhotos;
-};
-
-
 // Клонируем блок картинок
-var renderUsersPicture = function (photo) {
-  var usersPhotoElement = pictureTemplate.cloneNode(true);
+var renderPicture = function (photo){
+  var userPhotosElement = pictureTemplate.cloneNode(true);
 
-  usersPhotoElement.querySelector('.picture__comments').textContent = photo.comments;
-  usersPhotoElement.querySelector('.picture__likes').textContent = photo.likes;
-  usersPhotoElement.querySelector('.picture__img').src = photo.url;
-  usersPhotoElement.querySelector('.picture__img').alt = photo.description;
+  userPhotosElement.querySelector('.picture__img').src = userPhotos.url;
+  // usersPhotoElement.querySelector('.picture__comments').textContent = userPhotos.comments;
+  // usersPhotoElement.querySelector('.picture__likes').textContent = userPhotos.likes;
+  // usersPhotoElement.querySelector('.picture__img').alt = userPhotos.description;
 
-  return usersPhotoElement;
-};
+  return userPhotosElement;
+}
 
-var addListPictures = function (listPictures) {
-  var fragment = document.createDocumentFragment();
-
-  for (i = 0; i <= PICTURE_COUNT; i++) {
-    fragment.appendChild(renderUsersPicture(photos[i]));
-  }
-  listPictures.appendChild(fragment);
-};
-
+  picturesListElement.appendChild(usersPhotoElement);
